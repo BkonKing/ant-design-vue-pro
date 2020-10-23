280057f1b7968c9c030ca8fee1b81fa61c2b8107
hongbingkun <hongbingkun@qq.com>
2020-10-23 16:47:40 +0800

    feat：初始化

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import app from './modules/app'
+import user from './modules/user'
+
+// 默认路由器权限控制
+// import permission from './modules/permission'
+
+// 动态路由器权限控制(实验)
+import permission from './modules/async-router'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    app,
+    user,
+    permission
+  },
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters
+})
