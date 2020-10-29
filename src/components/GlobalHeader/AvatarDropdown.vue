0bea424c705ceb4fa61dfa6540165b116d0a698d
BkonKing <1810972990@qq.com>
2020-10-29 15:49:37 +0800

    fix：去除国际化

--- src/components/GlobalHeader/AvatarDropdown.vue
+++ src/components/GlobalHeader/AvatarDropdown.vue
@@ -51,8 +51,8 @@ export default {
     },
     handleLogout (e) {
       Modal.confirm({
-        title: this.$t('layouts.usermenu.dialog.title'),
-        content: this.$t('layouts.usermenu.dialog.content'),
+        title: '提示',
+        content: '是否退出登录？',
         onOk: () => {
           // return new Promise((resolve, reject) => {
           //   setTimeout(Math.random() > 0.5 ? resolve : reject, 1500)
